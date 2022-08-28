757aa0156305b1c0ff36a258aab060d2b8918a23
RaymanMoha <105370124+RaymanMoha@users.noreply.github.com>
2022-08-28 16:10:23 +0300

    Update 4-new_in_list.py

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    """Replaces an element in a list at a specific position
+    without modifying the original list (like in C)."""
+    new_list = my_list[:]
+    if 0 <= idx < len(new_list):
+        new_list[idx] = element
+        return (new_list)
+    return (my_list)
