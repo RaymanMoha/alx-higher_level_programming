1ec5f23677284f404b2de4ee2e0a178fdfa7d77d
RaymanMoha <abdulmoharayman@gmail.com>
2022-09-28 04:50:40 -0700

    append aftr

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,7 @@
+#!/bin/python3
+
+"""function that inserts a line of text to a file"""
+
+def append_after(filename="", search_string="", new_string=""):
+    with open(filename, mode="w", encoding="UTF-8") as f:
+        return f.write(text)
