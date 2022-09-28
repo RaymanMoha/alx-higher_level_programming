f7303ab4eda9631fa11390ff0a0a5e3e4a27c2ee
RaymanMoha <abdulmoharayman@gmail.com>
2022-09-28 23:09:15 +0000

    sqr

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 class square:
-<<<<<<< HEAD
 
-    def __init__(self):
+   def __init__(self):
 
-#empty square
+          """empty square"""
 
-=======
-"""empty square"""
->>>>>>> eb3f8bff9dfe11a80148bc2ea4aff5814f4a9aed
 pass
