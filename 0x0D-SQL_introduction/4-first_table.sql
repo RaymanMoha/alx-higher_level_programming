31351232c83c6f3984c06d977ab2e63771e66a99
RaymanMoha <105370124+RaymanMoha@users.noreply.github.com>
2022-11-03 15:31:57 +0300

    Create 4-first_table.sql

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+-- creates a table called first_table in the current database
+-- If the table first_table already exists, script should not fail
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
