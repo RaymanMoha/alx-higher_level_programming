66318f5c20dd0dc6b1a8838d89efd48cc3ebb540
RaymanMoha <105370124+RaymanMoha@users.noreply.github.com>
2022-12-16 11:33:40 +0300

    github commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Lists 10 commits (from the most recent to oldest) of the repository
+'rails' by the user 'rails'"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+            sys.argv[2], sys.argv[1])
+
+    r = requests.get(url)
+    commits = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
