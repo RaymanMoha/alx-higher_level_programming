e20c15a9598d378dc63040a42ab1d7fe0c9dcd20
RaymanMoha <105370124+RaymanMoha@users.noreply.github.com>
2023-01-11 14:51:58 +0300

    uniq add

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+    """Adds all unique integers in a list(only once for each integer)."""
+    sum = 0
+    for i in set(my_list):
+        sum += i
+    return sum
