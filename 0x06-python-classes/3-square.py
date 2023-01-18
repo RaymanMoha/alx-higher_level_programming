e170c3d45bfcdd4a4315a78d9b69113e50f8394c
RaymanMoha <105370124+RaymanMoha@users.noreply.github.com>
2023-01-18 12:08:16 +0300

    3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
+Square = __import__('3-square').Square
 
-class Square:
+my_square_1 = Square(3)
+print("Area: {}".format(my_square_1.area()))
 
-    def __init__(self, size=0)
+try:
+    print(my_square_1.size)
+except Exception as e:
+    print(e)
 
-self.__size = size
+try:
+    print(my_square_1.__size)
+except Exception as e:
+    print(e)
 
+my_square_2 = Square(5)
+print("Area: {}".format(my_square_2.area()))
